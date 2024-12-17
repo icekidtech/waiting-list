350a6d02235c03a0c8b3905cef6cbcc05aef246d
icekidtech <princeklvn7568@gmail.com>
2024-12-17 11:37:46 +0100

    New Commit

--- admin-dashboard/src/App.js
+++ admin-dashboard/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Dashboard from './pages/Dashboard';
+import Dashboard from './pages/Dashboard.js';
 import Users from './pages/Users';
 
 function App() {
